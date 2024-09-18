1743c59cbc9205ceb48130b3c42300f9b70fcf7e
Jonathan Moreno <morenoelectro@gmail.com>
2024-09-18 15:37:29 -0500

    add file

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from sqlalchemy.orm import Session
 from app.routes import user
 from app.utils.database import engine, Base
-from mangum import Mangum
 
 # Crear las tablas en la base de datos al iniciar
 Base.metadata.create_all(bind=engine)
 
+# Instanciar la aplicación de FastAPI
 app = FastAPI()
 
+# Incluir los routers de las rutas
 app.include_router(user.router)
 
 
-handler = Mangum(app)
+
