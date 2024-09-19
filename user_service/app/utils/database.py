be46e81509f7ec50af7e25ad36010d6b2e13607a
Jonathan Moreno <morenoelectro@gmail.com>
2024-09-19 08:47:15 -0500

    add file

--- user_service/app/utils/database.py
+++ user_service/app/utils/database.py
@@ -1,15 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
+from app.utils.config import SQLALCHEMY_DATABASE_URL
 
-load_dotenv()
-
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-
-if not SQLALCHEMY_DATABASE_URL:
-    raise ValueError("No se ha definido la URL de la base de datos en las variables de entorno")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,pool_size= 10, max_overflow= 30)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
