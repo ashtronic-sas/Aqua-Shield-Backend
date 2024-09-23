9cb6b679001a6c68a96d30bf8036c98c288f8852
jmoreno08 <morenoelectro@gmail.com>
2024-09-23 06:15:15 -0500

    Refactorizar carga de variables de entorno en jwt_handler.py y database.py

--- auth_service/app/auth/jwt_handler.py
+++ auth_service/app/auth/jwt_handler.py
@@ -1,10 +1,14 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from typing import Union
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = "47400c92991465683f995a754e4f9842cbb2aa1a518716b576dff3d2ab777851"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES =int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
--- auth_service/app/config/database.py
+++ auth_service/app/config/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin:LFS1ieqpA7bhuCK3W1Xo@db-aquashield-dev.c5guy4ya6433.us-east-2.rds.amazonaws.com:3306/dbaquashieldtest"
+from app.config.config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size= 10, max_overflow= 30)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
