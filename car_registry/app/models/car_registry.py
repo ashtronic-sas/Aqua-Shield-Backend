a34aca377218df09d3b77e2cd82eb301b7dc898e
jmoreno08 <morenoelectro@gmail.com>
2024-10-07 23:47:48 -0500

    refactor(car_registry): :recycle: Refactorizar modelo Car_Register en car_registry.py y servicio de registro de automóviles en car_registry.py

--- car_registry/app/models/car_registry.py
+++ car_registry/app/models/car_registry.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, String,DateTime, func
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime, func
 from sqlalchemy.orm import relationship
 from app.config.database import Base
 
@@ -8,11 +8,13 @@ class Car_Register(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     car_id = Column(Integer, ForeignKey("car.id"), nullable=False)
+    datetime = Column(DateTime(timezone=True), nullable=False)
     even_type = Column(String(50), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     car = relationship("Car", back_populates="car_register")
+
 
 class Car(Base):
     __tablename__ = "car"
@@ -25,22 +27,5 @@ class Car(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
-    user = relationship("Car", back_populates="owners")
     car_register = relationship("Car_Register", back_populates="car")
-    owners = relationship("Owner", back_populates="car")
 
-
-class Owner(Base):
-    __tablename__ = "owners"
-
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(100), nullable=False)
-    second_name = Column(String(100), nullable=True)
-    first_lastname = Column(String(100), nullable=False)
-    second_lastname = Column(String(100), nullable=True)
-    cedula = Column(String(50), unique=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
-
-    car = relationship("Owner", back_populates="car")
-    
--- car_registry/app/services/car_registry.py
+++ car_registry/app/services/car_registry.py
@@ -6,34 +6,41 @@ from app.schemas.car_registry import CarRegistryCreate, CarRegistryResponse
 def create_car_registry_db(db: Session, car: CarRegistryCreate):
     db_car = Car_Register(
         car_id=car.car_id,
+        datetime=car.datetime,
         even_type=car.even_type
     )
     db.add(db_car)
     db.commit()
     db.refresh(db_car)
-    return db_car
+    return db_car.__dict__  # Convertir el objeto a un diccionario
 
 def update_car_registry_db(db: Session, car_id: int, car: CarRegistryCreate):
     db_car = db.query(Car_Register).filter(Car_Register.id == car_id).first()
     if db_car is None:
         raise HTTPException(status_code=404, detail="Car not found")
-    db_car.car_id = car.car_id
-    db_car.even_type = car.even_type
+    
+    if car.car_id is not None:
+        db_car.car_id = car.car_id
+    if car.even_type is not None:
+        db_car.even_type = car.even_type
+    if car.datetime is not None:
+        db_car.datetime = car.datetime
+    
     db.commit()
     db.refresh(db_car)
-    return db_car
+    return db_car.__dict__
 
 def get_car_registry_by_id_db(db: Session, car_id: int):
     db_car = db.query(Car_Register).filter(Car_Register.id == car_id).first()
     if db_car is None:
         raise HTTPException(status_code=404, detail="Car not found")
-    return db_car
+    return db_car.__dict__
 
 def get_car_registry_by_car_id_db(db: Session, car_id: int):
     db_car = db.query(Car_Register).filter(Car_Register.car_id == car_id).all()
-    if db_car is None:
+    if not db_car:
         raise HTTPException(status_code=404, detail="Car not found")
-    return db_car
+    return [car.__dict__ for car in db_car]
 
 def delete_car_registry_db(db: Session, car_id: int):
     db_car = db.query(Car_Register).filter(Car_Register.id == car_id).first()
@@ -42,5 +49,3 @@ def delete_car_registry_db(db: Session, car_id: int):
     db.delete(db_car)
     db.commit()
     return {"message": "Car deleted"}
-
-
