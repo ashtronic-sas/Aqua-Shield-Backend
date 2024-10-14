6e190a1b71a5db38f65bd953c32a5df2e9dbacf5
Jonathan Moreno <morenoelectro@gmail.com>
2024-10-14 08:33:55 -0500

    feat(employee_register): :sparkles: add endpoint register_record

--- employee_register/app/routers/employee_register.py
+++ employee_register/app/routers/employee_register.py
@@ -3,8 +3,9 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.config.database import get_db
 from app.schemas.employee_register import EmployeeRegisterCreate, EmployeeRegisterUpdate, EmployeeRegisterResponse
-from app.services.employee_register import create_employee_register, update_employee_register, get_registers_by_employee, get_registers_by_place, delete_employee_register
+from app.services.employee_register import create_employee_register, update_employee_register, get_registers_by_employee, get_registers_by_place, delete_employee_register,parse_parameters
 from app.shared.utils import verify_token
+
 
 router = APIRouter(prefix="/employee_register", tags=["employee_register"])
 
@@ -36,24 +37,21 @@ def delete_register(id: int, db: Session = Depends(get_db)):
     return {"message": "EmployeeRegister deleted successfully"}
 
 @router.post("/register-record", response_description="register-record")
-async def register_record(request: Request):
-    print("register-record")
+async def register_record(request: Request, db: Session = Depends(get_db)):
+
     try:
         # Create a JSONResponse object to send as a response
         response = JSONResponse(
                 status_code=200,
                 content={"success": 'true', "result": 1},
             )
-        # Get the request body data as bytes
+
         databyte = await request.body()
-        # Decode the bytes data as a string
         strRecord = databyte.decode("UTF-8")
-        # Enviar el callback a procesar en segundo plano
-        #background_tasks.add_task(save_attendance,databyte)
-        #await save_attendance(strRecord)
-        print(strRecord)
+        parse_parameters(db,strRecord)
+
         return response
-    # If any errors occur during the process, catch them and return a JSONResponse with an error message
+
     except Exception as e:
         response = JSONResponse(
             status_code=500,
